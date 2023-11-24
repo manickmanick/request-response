c5c81b52ebc897b75fff2ce0dc4619e99e9349f0
manickmanick <manickaperumal22@gmail.com>
2023-11-24 18:05:27 +0530

    added guards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+  // @UseGuards()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
